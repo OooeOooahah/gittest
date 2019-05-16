f71dacea37ec47e4453ece3465c49745ff5a7056
Crash <Mr_Xiao_Wu@outlook.com>
2019-05-16 23:44:10 +0800

    println

--- src/main/java/com/git/helloword.java
+++ src/main/java/com/git/helloword.java
@@ -7,7 +7,7 @@ package com.git;
 public class helloword {
     public static void main(String[] args) {
         System.out.println("hello");
-        System.out.println("world");
+        System.out.print("worldbbb");
         System.out.println("hello");
         for (int i = 0; i < 10; i++) {
             System.out.println(i);
