30626c564157639d39452d32676f06d59b40be1d
Crash <Mr_Xiao_Wu@outlook.com>
2019-05-16 23:22:01 +0800

    commit to remote resporty

---
+++ src/main/java/com/git/helloword.java
@@ -0,0 +1,13 @@
+package com.git;
+
+/**
+ * @author 保温杯泡枸杞
+ * @Created at time  2019-05-16 下午 11:17 星期四
+ */
+public class helloword {
+    public static void main(String[] args) {
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+    }
+}
