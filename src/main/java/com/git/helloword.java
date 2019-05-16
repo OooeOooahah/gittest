fadb928053d068c1d217c16960f427a7cd322a68
Crash <Mr_Xiao_Wu@outlook.com>
2019-05-16 23:41:06 +0800

    循环

--- src/main/java/com/git/helloword.java
+++ src/main/java/com/git/helloword.java
@@ -7,7 +7,10 @@ package com.git;
 public class helloword {
     public static void main(String[] args) {
         System.out.println("hello");
+        System.out.println("world");
         System.out.println("hello");
-        System.out.println("hello");
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
     }
 }
