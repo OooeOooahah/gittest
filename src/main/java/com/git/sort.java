168b55bcdb4e2495d434facd623704620ec6257f
Crash <Mr_Xiao_Wu@outlook.com>
2019-05-17 10:12:54 +0800

    排序代码

---
+++ src/main/java/com/git/sort.java
@@ -0,0 +1,13 @@
+package com.git;
+
+/**
+ * @author 保温杯泡枸杞
+ * @Created at time  2019-05-17 上午 10:10 星期五
+ */
+public class sort {
+    public static void main(String[] args) {
+        int [] a =new int[]{};
+        a= new int[]{1, 2, 3};
+        System.out.println(a);
+    }
+}
